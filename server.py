04492f236ae5b7eafab31f69173438016cf8baa7
Pratik Patil <87142731+ppatil002@users.noreply.github.com>
2023-05-07 04:35:34 +0000

    Completed Recipe Recommendation table

--- server.py
+++ server.py
@@ -5,81 +5,94 @@ from scipy import spatial
 import config
 import recommend_recipe
 
+
 def get_suggestions():
     data = pd.read_csv('cuisines.csv')
     result = list(data['Dish Name'])
     return result
 
+
 def Cosine_Similarity(Id1, Id2):
-    cuisine_df = pd.read_csv('C:/Prerna Tulsiani/COEP/Sem6/Data Science Project/Final Project/Bin_cuisines.csv')
+    cuisine_df = pd.read_csv(
+        'C:/Prerna Tulsiani/COEP/Sem6/Data Science Project/Final Project/Bin_cuisines.csv')
     A = json.loads(cuisine_df['Bin Ingredients'][Id1])
     B = json.loads(cuisine_df['Bin Ingredients'][Id2])
-    distance=spatial.distance.cosine(A,B)
-    
+    distance = spatial.distance.cosine(A, B)
+
     return distance, Id2
+
 
 def get_similar_recipes(dish_name):
     print("Entered Similar Recipes function")
-    food=[]
-    cuisine_df = pd.read_csv('C:/Prerna Tulsiani/COEP/Sem6/Data Science Project/Final Project/Bin_cuisines.csv')
+    food = []
+    cuisine_df = pd.read_csv(
+        'C:/Prerna Tulsiani/COEP/Sem6/Data Science Project/Final Project/Bin_cuisines.csv')
     found_index = 0
 
     for iterator in range(len(cuisine_df.index)):
-        if(iterator in cuisine_df.index):
-            if(cuisine_df['Dish Name'][iterator] == dish_name):
+        if (iterator in cuisine_df.index):
+            if (cuisine_df['Dish Name'][iterator] == dish_name):
                 found_index = iterator
 
     for i in cuisine_df.index:
-        food.append(Cosine_Similarity(found_index,i))
-        if(i==30):
+        food.append(Cosine_Similarity(found_index, i))
+        if (i == 30):
             break
 
-    common_ingredients=sorted(food,key=lambda x: x[0])[1:10]
+    common_ingredients = sorted(food, key=lambda x: x[0])[1:10]
     indexes = []
     for i in range(len(common_ingredients)):
         indexes.append(common_ingredients[i][1])
-    
+
     print(indexes)
     return indexes
 
+
 def get_full_information_recipe(dishes_array):
-    cuisine_df = pd.read_csv('C:/Prerna Tulsiani/COEP/Sem6/Data Science Project/Final Project/Bin_cuisines.csv')
-    cuisine_dict = dict()
+    cuisine_df = pd.read_csv(
+        'C:/Prerna Tulsiani/COEP/Sem6/Data Science Project/Final Project/Bin_cuisines.csv')
+    cuisine_dict = []
     print(dishes_array)
     for iterator in cuisine_df.index:
-        if(cuisine_df['Dish Name'][iterator] in dishes_array):
-            cuisine_dict[cuisine_df['Cuisine'][iterator]] = [cuisine_df['Dish Name'][iterator], cuisine_df['Ingredients'][iterator]]
-    
-    for k in cuisine_dict:
-        print(k, cuisine_dict)
+        if (cuisine_df['Dish Name'][iterator] in dishes_array):
+            cuisine_dict.append([cuisine_df['Cuisine'][iterator], cuisine_df['Dish Name'][iterator],
+                                cuisine_df['Ingredients'][iterator], cuisine_df['Calories'][iterator]])
+
+    return cuisine_dict
 
 # Flask API
+
 
 ing_list_recipe = []
 ing_count_flag = 0
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+
 @app.route("/search")
 def search():
     suggestions = get_suggestions()
-    return render_template('search.html',data=json.dumps(suggestions))
+    return render_template('search.html', data=json.dumps(suggestions))
 
-@app.route("/similar_recipe",methods=["POST"])
+
+@app.route("/similar_recipe", methods=["POST"])
 def similar_recipe():
     dish_name = request.form['name']
     indexes = get_similar_recipes(dish_name)
     return "hello pratik"
 
+
 @app.route("/similar")
 def similar():
     suggestions = get_suggestions()
-    return render_template('similar.html',data=json.dumps(suggestions))
+    return render_template('similar.html', data=json.dumps(suggestions))
+
 
 @app.route("/search_recipe_ingredients", methods=["POST"])
 def search_recipe_ingredients():
@@ -87,11 +100,14 @@ def search_recipe_ingredients():
     ing_list_recipe = ing_list.split("+")
     ing_list_recipe = " ".join(ing_list_recipe)
     dishes_array = recommend_recipe.RecSys(ing_list_recipe)
-    get_full_information_recipe(dishes_array)
+    recommended_recipes = get_full_information_recipe(dishes_array)
+    return recommended_recipes
+
 
 @app.route("/recipe_recommend")
 def recipe_recommend():
     return render_template('recipe_recommend.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/ingredient_list.js
+++ static/ingredient_list.js
@@ -13,46 +13,114 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
+  };
+}
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
   }
 }
 
 // Click on Search Recipes button
-function search(){
+function search() {
   var ingredients = [];
-  for (i = 0;  i < myNodelist.length; i++){
-    var lines = myNodelist[i].innerText
-    ingredients.push(lines.substring(lines.lastIndexOf("\n") + 1, -1 ).replace(/\n/g, ''))
+
+  for (i = 0; i < myNodelist.length; i++) {
+    var lines = myNodelist[i].innerText;
+    ingredients.push(
+      lines.substring(lines.lastIndexOf("\n") + 1, -1).replace(/\n/g, "")
+    );
   }
-  console.log(ingredients)
+  console.log(ingredients);
   ing_str = ingredients.join("+");
   $.ajax({
-    type: 'POST',
+    type: "POST",
     url: "/search_recipe_ingredients",
-    data:{'ing_array':ing_str},
-    success: function(recs){
-        console.log(recs)
-    }
-})
+    data: { ing_array: ing_str },
+    success: function (recs) {
+      let recommended_recipes = [
+        {
+          Cuisine: recs[0][0],
+          Recipe: recs[0][1],
+          Ingredients: recs[0][2],
+          Calories: recs[0][3],
+        },
+        {
+          Cuisine: recs[1][0],
+          Recipe: recs[1][1],
+          Ingredients: recs[1][2],
+          Calories: recs[1][3],
+        },
+        {
+          Cuisine: recs[2][0],
+          Recipe: recs[2][1],
+          Ingredients: recs[2][2],
+          Calories: recs[2][3],
+        },
+        {
+          Cuisine: recs[3][0],
+          Recipe: recs[3][1],
+          Ingredients: recs[3][2],
+          Calories: recs[3][3],
+        },
+        {
+          Cuisine: recs[4][0],
+          Recipe: recs[4][1],
+          Ingredients: recs[4][2],
+          Calories: recs[4][3],
+        },
+      ];
+
+      let table = document.querySelector("table");
+      let data = Object.keys(recommended_recipes[0]);
+      generateTableHead(table, data);
+      generateTable(table, recommended_recipes);
+      console.log(recommended_recipes);
+      window.scrollTo(0, document.body.scrollHeight);
+    },
+  });
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
+  $("#table_of_items tr").remove();
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -66,9 +134,9 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
